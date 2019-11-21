54ace3c1364f9a95022fc9529391dce2d052a500
João Paulo <joaoprrn@hotmail.com>
2019-11-20 20:41:50 -0400

    arrumamos a função ordena arquivo

--- arquivo.c
+++ arquivo.c
@@ -21,21 +21,21 @@ void gerar_arq_aleat(int quant){
 
 
 
-/*int* ordena_arquivo(int *vet){
+void ordena_arquivo(int *vet, int tamanhoArquivo){
     FILE *file;
     file = fopen("arquivo.txt","r");
 
     if(file == NULL){
         printf("O arquivo não pôde ser aberto\n");
-        return 0;
+        return;
     }
     
 
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < tamanhoArquivo; i++){
         fscanf(file,"%d",&vet[i]);
         // insere numeroa do arquivo no vetor
 
     }
-    return vet;
-}*/
+    //return vet;
+}
 
